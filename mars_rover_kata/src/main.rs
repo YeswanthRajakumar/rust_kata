c1d5f9a41b5f14713324ed7a5cc5c32f92cde1f5
Yeswanth Rajakumar <yeswanth.rajakumar@vernacular.ai>
2021-12-18 12:57:23 +0530

    removed comments

--- mars_rover_kata/src/main.rs
+++ mars_rover_kata/src/main.rs
@@ -11,9 +11,7 @@ use rover::Rover;
 
 fn main() {
     let p1 = Plateau::new();
-    // println!("{:#?}", p1)
     let mut rover = Rover::new(String::from("1 2 N"));
-    // println!("{:#?}",rover);
     let instructions = String::from("LMLMLMLMM");
     let final_result = rover.execute(instructions);
     println!("{}", final_result)
